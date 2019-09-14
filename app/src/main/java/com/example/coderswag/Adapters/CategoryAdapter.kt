228a96a7106a7c303220e5eebb98e115af527056
brahmadatta <g.brahmadatta@gmail.com>
2019-09-14 23:18:13 +0530

    lesson-04

--- app/src/main/java/com/example/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryAdapter.kt
@@ -20,22 +20,32 @@ class CategoryAdapter(context : Context,categories : List<Category>) : BaseAdapt
 
         val categoryView : View
 
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item,null)
+        val  holder : ViewHolder
 
-        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
+        if (convertView == null){
 
-        val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item,null)
 
-        println("Heavy Computing")
+            holder = ViewHolder()
+
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+            categoryView?.tag = holder
+        }else{
+             holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
+
+
 
 
         val category = categories[position]
 
         val resourceId = context.resources.getIdentifier(category.image,"drawable",context.packageName)
-        categoryImage.setImageResource(resourceId)
-        println(resourceId)
-
-        categoryName.text = category.title
+        holder.categoryImage?.setImageResource(resourceId)
+        holder.categoryName?.text = category.title
 
         return categoryView
 
@@ -53,5 +63,10 @@ class CategoryAdapter(context : Context,categories : List<Category>) : BaseAdapt
     override fun getCount(): Int {
 
         return categories.count()
+    }
+
+    private class ViewHolder{
+        var categoryImage : ImageView?= null
+        var categoryName : TextView?= null
     }
 }
