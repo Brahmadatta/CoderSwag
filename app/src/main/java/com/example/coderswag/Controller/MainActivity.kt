bb78f8df7a04855c2fc1b58ee7dee93e4c729bc0
brahmadatta <g.brahmadatta@gmail.com>
2019-09-15 22:29:53 +0530

    Refactored for recyclerView adpter

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coderswag.Adapters.CategoryAdapter
+import com.example.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataService
@@ -14,7 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     //lateinit var adapter : ArrayAdapter<Category>
 
-    lateinit var adapter : CategoryAdapter
+
+    //lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +26,14 @@ class MainActivity : AppCompatActivity() {
 
         //adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,DataService.categories)
 
-        adapter = CategoryAdapter(this,DataService.categories)
+        //adapter = CategoryAdapter(this,DataService.categories)
+        adapter = CategoryRecycleAdapter(this,DataService.categories)
 
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
         //for listView click listener.
 //        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
